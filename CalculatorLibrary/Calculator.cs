4c1aa17264cc10b420619811e9a3963347a67027
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-10-02 13:07:48 +0200

    Added all the libraries and test methods

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static int Add(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Subtract(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Multiply(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Divide(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Sum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Minimum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Maximum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object Average(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmartMenuLibrary/SmartMenu.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenuLibrary
+{
+    public class SmartMenu
+    {
+
+        string line;
+        List<string> lines = new List<string>();
+        string titel = "";
+        string explain = "";
+        string[,] menu;
+        public void LoadMenu(string path)
+        {
+            int counter = 0;
+
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\..\MenuSpec.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+                counter++;
+            }
+
+
+            titel = lines[0];
+            explain = lines[1];
+
+
+            //indlæs menupunkter
+            menu = new string[lines.Count - 2, 2];
+            for (int i = 2; i < lines.Count; i++)
+            {
+                string[] split = lines[i].Split(';');
+
+                menu[i - 2, 0] = split[0];
+                menu[i - 2, 1] = split[1];
+
+            }
+
+
+            file.Close();
+
+
+        }
+        public void Activate()
+        {
+
+            int choice = 0;
+            bool running = true;
+            while (running != false)
+            {
+
+                Console.Clear();
+
+                Console.WriteLine(titel + "\n");
+
+                Console.WriteLine(explain + "\n");
+
+                // print alle menuerne
+                for (int i = 0; i < menu.GetLength(0); i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + menu[i, 0] + "\n");
+                }
+
+                string input = Console.ReadLine();
+
+
+                if (input.ToLower() == "0")
+                {
+                    running = false;
+                }
+                else if (int.TryParse(input, out choice))
+                {
+                    choice -= 1;
+                    //kald bindings klassen
+                    if (choice < menu.GetLength(0))
+                    {
+                        Bindings.Call(menu[choice, 1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number you put in is too high, choose one from the description");
+
+                    }
+                }
+
+                else if (input.Length <= 0)
+                {
+                    Console.WriteLine("Please enter a number");
+                }
+                else
+                {
+                    Console.WriteLine("that is not a number, please enter a number ");
+                }
+
+                Console.ReadLine();
+
+
+
+
+
+            }
+        }
+    }
+}
